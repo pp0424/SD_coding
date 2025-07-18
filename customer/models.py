90cb3cb9ebf7ff931f439aaeacee621a9290b278
pp0424 <hpp15659224276@gmail.com>
2025-07-18 19:29:39 +0800

    Update models.py

--- customer/models.py
+++ customer/models.py
@@ -3,7 +3,6 @@ from database import db
 
 class Customer(db.Model):
     __tablename__ = 'customers'
-    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
     phone = db.Column(db.String(50))
