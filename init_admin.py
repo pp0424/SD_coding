c9ded05ed09193beed4171aa3f24ca28ded70ad2
lllsssss <151696830+lllsssss@users.noreply.github.com>
2025-08-13 00:52:43 +0800

    Create init_admin.py
    Signed-off-by: lllsssss <151696830+lllsssss@users.noreply.github.com>

---
+++ init_admin.py
@@ -0,0 +1,11 @@
+from app import app, db, User
+from werkzeug.security import generate_password_hash
+
+with app.app_context():  # 确保有 Flask 应用上下文
+    admin = User(
+        username='admin',
+        password=generate_password_hash('123456'),
+    )
+    db.session.add(admin)
+    db.session.commit()
+    print("管理员用户创建成功")
