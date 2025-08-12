c6054d4ec431da6514aade5f0de7574271f123bd
lllsssss <151696830+lllsssss@users.noreply.github.com>
2025-08-13 01:08:03 +0800

    Update init_admin.py
    Signed-off-by: lllsssss <151696830+lllsssss@users.noreply.github.com>

--- init_admin.py
+++ init_admin.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash
 with app.app_context():  # 确保有 Flask 应用上下文
     admin = User(
         username='admin',
-        password=generate_password_hash('123456'),
+        password_hash=generate_password_hash('123456'),
     )
     db.session.add(admin)
     db.session.commit()
